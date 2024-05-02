9dc18ec5059bdaa84dbac66f1670f3cc242a4095
cloud <clloud3@naver.com>
2024-05-02 18:08:04 +0900

    세팅 완료

---
+++ src/main/java/com/kda/kdagg/KdaggApplication.java
@@ -0,0 +1,13 @@
+package com.kda.kdagg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdaggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdaggApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kda/kdagg/main/KdaMainController.java
@@ -0,0 +1,13 @@
+package com.kda.kdagg.main;
+
+import org.springframework.stereotype.Controller;
+
+
+// spring이 con.kda.kdagg 패키지 이하를 스캔해서 모든 파일을 메모리에 new 하는 것이 아님
+// 특정 어노테이션이 붙어있는 클래스 파일들을 new해서 (IOC) 스프링 컨테이너에서 관리함
+@Controller 
+public class KdaMainController {
+	
+
+
+}
